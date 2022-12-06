cd2d14d2f94d3ff13c435ceaeb49cd18efcb90b9
Patrobas Bwire <patrobasbwire@gmail.com>
2022-12-06 17:48:29 +0300

    created navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Container, Button} from 'react-bootstrap'
+
+
+class Home extends React.Component {
+  state = {
+    showModal: false
+  }
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal })
+  }
+
+  render() {
+    return(
+      <Container className="p-5 mt-4 bg-light text-center rounded shadow">
+        <h1 className='display-5 fw-bold'>Welcome to BookCritiq</h1>
+        <p className='fs-4'>Helping fellow readers choose their next book by offering honest reviews.</p>
+        <Button className="btn btn-secondary btn-lg shadow m-3" onClick={this.toggleModal}>Sign Up</Button>
+        <Link to='/books'><Button className='btn btn-secondary btn-lg shadow' type='button'>Find a Book To Review</Button></Link>
+
+      </Container>
+    )
+  }
+
+}
+
+export default Home
