9c4343d42b85b46e118539e2553c21ab9ba877aa
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-12-07 11:59:01 +0300

    add review

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -6,6 +6,22 @@ import {Container, Card, Button} from 'react-bootstrap';
 const Reviews = (props) => {
 
     const handleDelete = (review) => {
-      props.deleteReview(review.id, review.book_id);
+      props.deleteReview(review.id, review.hub_id);
     }
+
+    return (
+        <Container>
+          <h2 className='header text-center bg-primary text-white'>Reviews</h2>
+          {props.reviews.map(review => 
+            <Card key={review.id} className='border-dark p-2 mb-2 shadow'>
+              <Card.Title>{review.title}</Card.Title>
+              <Card.Text>{review.content}</Card.Text>
+              <Button variant='secondary' className='m-auto shadow' onClick={() => handleDelete(review)}>Delete</Button>
+            </Card>
+          )}
+        </Container>
+      )
+    }
+    
+    export default connect(null, {deleteReview})(Reviews);
   
