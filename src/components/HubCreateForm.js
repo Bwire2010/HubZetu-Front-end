210a6254b174fe40aedcf85907ad128034e890fc
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-12-07 08:29:52 +0300

    add hubcreate form

---
+++ src/components/HubCreateForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+import {connect} from 'react-redux';
+import {addHub} from '../actions/addHub.js'
+
+class HubCreateForm extends React.Component {
+    
+}
